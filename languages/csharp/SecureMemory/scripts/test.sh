98f7f137cd67c61939ccf1d4bce81609d40916a9
Nikhil Lohia <nikoo28@gmail.com>
2019-07-24 14:39:37 -0700

    Change code coverage o/p format to opencover for all projects

--- languages/csharp/SecureMemory/scripts/test.sh
+++ languages/csharp/SecureMemory/scripts/test.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Have to explicitly exclude xunit from coverage. Excluding all MacOS related files
-dotnet test --configuration Release --no-build /p:CollectCoverage=true /p:Exclude=\"[xunit*]*,[*.Tests]*\" /p:CoverletOutputFormat=cobertura /p:ExcludeByFile="../**/MacOS/**/*.cs"
+dotnet test --configuration Release --no-build /p:CollectCoverage=true /p:Exclude=\"[xunit*]*,[*.Tests]*\" /p:CoverletOutputFormat=opencover /p:ExcludeByFile="../**/MacOS/**/*.cs"
