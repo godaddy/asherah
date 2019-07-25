1eaa54746a8c7fc245d5ab609ec95991f0ae62db
Nikhil Lohia <nikoo28@gmail.com>
2019-07-25 14:42:13 -0700

    Added trx logger to all test scripts Update circleci configuration to use the generated testresults for all csharp projects

--- languages/csharp/AppEncryption/scripts/test.sh
+++ languages/csharp/AppEncryption/scripts/test.sh
@@ -3,4 +3,4 @@ set -e
 
 # Have to explicitly exclude xunit and test projects from coverage. Excluding all MacOS related files
 # Using the FullyQualifiedName filter to run the unit tests only
-dotnet test --filter FullyQualifiedName~AppEncryption.Tests --configuration Release --no-build /p:CollectCoverage=true /p:Exclude=\"[xunit*]*,[*.IntegrationTests]*,[*.Tests]*\" /p:CoverletOutputFormat=opencover /p:ExcludeByFile="../**/MacOS/**/*.cs"
+dotnet test --filter FullyQualifiedName~AppEncryption.Tests --configuration Release --logger "trx" --no-build /p:CollectCoverage=true /p:Exclude=\"[xunit*]*,[*.IntegrationTests]*,[*.Tests]*\" /p:CoverletOutputFormat=opencover /p:ExcludeByFile="../**/MacOS/**/*.cs"
