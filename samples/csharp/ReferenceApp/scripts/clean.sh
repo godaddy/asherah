77803da29f474f197943c0293b39cf1d48674148
Nikhil Lohia <nikoo28@gmail.com>
2019-07-31 17:24:01 -0700

    No longer hacky way needed

--- samples/csharp/ReferenceApp/scripts/clean.sh
+++ samples/csharp/ReferenceApp/scripts/clean.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Hacky work-around to support pulling updated alpha/snapshot builds
-dotnet nuget locals global-packages --clear && mkdir -p $HOME/.nuget/packages
-
 git clean -xfd
 dotnet clean --configuration Release
 dotnet restore
