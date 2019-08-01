cebfa55ef5e095018b1444b65772af44e2457835
Nikhil Lohia <nikoo28@gmail.com>
2019-08-01 15:51:56 -0700

    Fix versioning

--- languages/csharp/Logging/scripts/install_local.sh
+++ languages/csharp/Logging/scripts/install_local.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-dotnet pack -c Release --version-suffix alpha-$(date "+%Y%m%d")
-find . -name *.nupkg -exec dotnet nuget push {} -s local \;
+FULL_VERSION=$(xmllint --xpath "//Project/PropertyGroup/Version/text()" Directory.Build.props)
+TIME_STAMP=.$(date "+%Y%m%d")
+PACKAGE_VERSION=${FULL_VERSION}.${TIME_STAMP}
+
+dotnet pack -c Release -p:PackageVersion=${PACKAGE_VERSION}
+find . -name *${PACKAGE_VERSION}.nupkg -exec dotnet nuget push {} -s local \;
