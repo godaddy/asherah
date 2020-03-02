38083c1eb0e5c5b69efe0caa7c603a6019ce36be
Nikhil Lohia <nikoo28@gmail.com>
2020-03-02 14:58:19 -0700

    It works

--- tests/cross-language/scripts/test.sh
+++ tests/cross-language/scripts/test.sh
@@ -5,12 +5,16 @@ mkdir -p encrypted_files
 
 cd java
 mvn -Dtest=RunEncryptTest test
-mv java_encrypted ../encrypted_files
+mv java_encrypted ../encrypted_files/
 
-cd ../csharp
+cd ..
+
+cd csharp
 cp ../features/* .
 dotnet test --filter FullyQualifiedName=csharp.EncryptDataUsingInMemoryMetastoreStaticKMSFeature.EncryptingData
 mv bin/Debug/csharp_encrypted ../encrypted_files/
 
-#cd ../go
+cd ..
+
+#cd go
 # run go encrypt tests here
