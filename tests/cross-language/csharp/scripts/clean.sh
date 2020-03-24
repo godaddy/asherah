8608ef1e3e003af8a4a5bb47eca8d5a2232ebcb3
Sushant Mimani <smimani@godaddy.com>
2020-03-24 15:04:42 -0700

    Update CICD

--- tests/cross-language/csharp/scripts/clean.sh
+++ tests/cross-language/csharp/scripts/clean.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-dotnet build --no-restore
+dotnet clean
+dotnet restore
--- tests/cross-language/scripts/test.sh
+++ tests/cross-language/scripts/test.sh
@@ -4,35 +4,35 @@ set -e
 
 # Run encrypt tests for all languages
 cd java
-echo "Encrypting payload using Java"
+echo "----------------------Encrypting payload using Java---------------------"
 mvn -Dtest=RunEncryptTest test
 cd ..
 
 cd csharp
-echo "Encrypting payload using C#"
+echo "----------------------Encrypting payload using C#-----------------------"
 cp ../features/* .
-dotnet test --filter FullyQualifiedName=GoDaddy.Asherah.Cltf.EncryptDataUsingRDBMSMetastoreAndStaticKMSFeature.EncryptingData --no-build
+dotnet test --filter FullyQualifiedName=GoDaddy.Asherah.Cltf.EncryptDataUsingRDBMSMetastoreAndStaticKMSFeature.EncryptingData
 cd ..
 
 cd go
-echo "Encrypting payload using Go"
+echo "----------------------Encrypting payload using Go-----------------------"
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 godog ../features/encrypt.feature
 cd ..
 
 # Run decrypt tests for all languages
 cd java
-echo "Decrypting data using Java"
+echo "----------------------Decrypting data using Java------------------------"
 mvn -Dtest=RunDecryptTest test
 cd ..
 
 cd csharp
-echo "Decrypting data using C#"
-dotnet test --filter FullyQualifiedName=GoDaddy.Asherah.Cltf.DecryptDataUsingRDBMSMetastoreAndStaticKMSFeature.DecryptingData --no-build
+echo "----------------------Decrypting data using C#--------------------------"
+dotnet test --filter FullyQualifiedName=GoDaddy.Asherah.Cltf.DecryptDataUsingRDBMSMetastoreAndStaticKMSFeature.DecryptingData
 rm *.feature
 cd ..
 
 cd go
-echo "Decrypting data using Go"
+echo "----------------------Decrypting data using Go--------------------------"
 godog ../features/decrypt.feature
 cd ..
