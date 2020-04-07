4d881f71119011f060c8715bf35de901ec7e11db
Nikhil Lohia <nlohia@godaddy.com>
2020-04-07 16:19:14 -0700

    Add Java client for servicelayer

---
+++ server/samples/clients/java/src/test/java/com/godaddy/asherah/client/AppEncryptionClientTest.java
@@ -0,0 +1,33 @@
+package com.godaddy.asherah.client;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppEncryptionClientTest extends TestCase {
+  /**
+   * Create the test case
+   *
+   * @param testName name of the test case
+   */
+  public AppEncryptionClientTest(String testName) {
+    super(testName);
+  }
+
+  /**
+   * @return the suite of tests being tested
+   */
+  public static Test suite() {
+    return new TestSuite(AppEncryptionClientTest.class);
+  }
+
+  /**
+   * Rigourous Test :-)
+   */
+  public void testApp() {
+    assertTrue(true);
+  }
+}
