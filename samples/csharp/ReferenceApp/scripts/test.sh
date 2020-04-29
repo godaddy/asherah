92dbdafb6b6c8dbc43559d2f86f85c5d9ceef386
Sushant Mimani <smimani@godaddy.com>
2020-04-29 15:03:39 -0700

    Delete unused scripts

--- samples/csharp/ReferenceApp/scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Have to explicitly exclude xunit from coverage. Excluding all MacOS related files
-dotnet test --configuration Release --no-build /p:CollectCoverage=true /p:Exclude=\"[xunit*]*,[*.Tests]*\" /p:CoverletOutputFormat=cobertura /p:ExcludeByFile="../**/MacOS/**/*.cs"
