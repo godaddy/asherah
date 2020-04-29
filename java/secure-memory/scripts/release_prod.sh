980913f16d96082a26598dd016ae23b10377b044
Sushant Mimani <smimani@godaddy.com>
2020-04-29 12:31:47 -0700

    Add deploy scripts and new circleci config for java

--- java/secure-memory/scripts/release_prod.sh
+++ java/secure-memory/scripts/release_prod.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
-FULL_VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
-BASE_VERSION=`echo $FULL_VERSION | cut -f1 -d'-'`
-echo $BASE_VERSION
-BRANCH="release-${BASE_VERSION}"
-NEW_VERSION="${BASE_VERSION}"
-echo $NEW_VERSION
+set -e
 
-git fetch
-git checkout "${BRANCH}"
-git pull origin "${BRANCH}"
-mvn versions:set versions:commit -DnewVersion="${NEW_VERSION}"
-git add pom.xml
-git commit -m "Production release version bump to ${NEW_VERSION}"
-TAG="v${NEW_VERSION}"
-git tag -f ${TAG}
-git push origin "${BRANCH}"
-git push origin ${TAG}
+BASE_VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
+ARTIFACT_NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.artifactId)
+TAG=`echo java/${ARTIFACT_NAME}/v${BASE_VERSION}`
+
+RESULT=$(git tag -l ${TAG})
+if [[ "$RESULT" != ${TAG} ]]; then
+    echo ${PRIVATE_GPG_KEY} | base64 --decode | gpg --batch --no-tty --import --yes
+    echo "Release prod artifact"
+    mvn -DskipTests -s ../../.circleci/settings.xml deploy -Prelease
+
+    # Create tag
+    git tag -f ${TAG} ${CIRCLE_SHA1}
+    git push origin ${TAG}
+else
+    echo "Module is already deployed and tagged"
+fi
