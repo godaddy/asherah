b2560a187ac388bdb8b8ba390e74bcbc8f00d251
Sushant Mimani <smimani@godaddy.com>
2020-05-04 16:02:19 -0700

    Update circleci config and POM files

--- go/securememory/scripts/release_prod.sh
+++ go/securememory/scripts/release_prod.sh
@@ -10,7 +10,7 @@ if [[ "$RESULT" != ${TAG}  ]]; then
     # Create tag
     echo "Releasing ${ARTIFACT_NAME} artifact"
     git tag -f ${TAG} ${CIRCLE_SHA1}
-    git push origin ${TAG}
+    ssh-agent sh -c 'ssh-add ~/.ssh/id_rsa_git; git push origin --tags'
     echo "Created tag ${TAG}"
 else
     echo "${TAG} exists for ${ARTIFACT_NAME} v${BASE_VERSION}"
