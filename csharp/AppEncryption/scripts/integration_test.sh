2e41a8d82a09067eb89a0aca39b70aa48e27938d
Sushant Mimani <smimani@godaddy.com>
2020-05-05 10:23:46 -0700

    Enable artifact release (#146)
    * JAVA based grpc server implementation
    * Add docker image
    * Add deploy scripts and new circleci config for java
    * Add test case for multiple clients
    * add go changes
    * add csharp and other changes
    * add * from c# dependencies
    * Suppress errors for editors like vscode which don't read pom checkstyle configurations
    * Run C# AE integration tests without coverage
    * Delete unused scripts
    * correct script name[ci skip]
    * trigger build
    * Address review comments
    * update circleci config
    * Add GH keys manually if deploying
    * Update circleci config and release scripts
    * Update circleci config and POM files
    
    Co-authored-by: Nikhil Lohia <nikoo28@gmail.com>

--- csharp/AppEncryption/scripts/integration_test.sh
+++ csharp/AppEncryption/scripts/integration_test.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Have to explicitly exclude xunit and test projects from coverage. Excluding all MacOS related files
-# Using the FullyQualifiedName filter to run the integration tests only
-dotnet test --filter FullyQualifiedName~AppEncryption.IntegrationTests --configuration Release --no-build /p:CollectCoverage=true /p:Exclude=\"[xunit*]*,[*.IntegrationTests]*,[*.Tests]*\" /p:CoverletOutputFormat=cobertura /p:ExcludeByFile="../**/MacOS/**/*.cs"
+# Using the FullyQualifiedName filter to run the integration tests only. Do not record coverage
+dotnet test --filter FullyQualifiedName~AppEncryption.IntegrationTests --configuration Release --logger "trx" --no-build
