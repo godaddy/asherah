3c37810c943f4e44255734622282c6f155e2c3c5
Nikhil Lohia <nikoo28@gmail.com>
2020-05-18 18:09:31 -0700

    Enable builds for sample clients

---
+++ server/samples/clients/test_clients.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -e
+
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+export ASHERAH_SERVICE_NAME=service
+export ASHERAH_PRODUCT_NAME=product
+export ASHERAH_KMS_MODE=static
+export ASHERAH_METASTORE_MODE=memory
+
+# Test Go server
+cd ../../go
+export ASHERAH_EXPIRE_AFTER=60m
+export ASHERAH_CHECK_INTERVAL=10m
+go run main.go -s /tmp/appencryption.sock &
+ASHERAH_GO_SIDECAR_PID=$!
+cd -
+sleep 10
+
+cd python
+pip3.7 install -r requirements.txt
+python3.7 appencryption_client.py
+cd ..
+
+cd node
+npm install
+node appencryption_client.js
+cd ..
+
+kill $ASHERAH_GO_SIDECAR_PID
+rm -rf /tmp/appencryption.sock
+
+# Test Java server
+# TODO : Remove this after unifying configurations
+# https://github.com/godaddy/asherah/issues/143
+export ASHERAH_EXPIRE_AFTER=90
+export ASHERAH_CHECK_INTERVAL=10
+java -jar ~/.m2/repository/com/godaddy/asherah/grpc-server/1.0.0-SNAPSHOT/*dependencies.jar &
+ASHERAH_JAVA_SIDECAR_PID=$!
+sleep 10
+
+cd python
+python3.7 appencryption_client.py
+cd ..
+
+cd node
+node appencryption_client.js
+cd ..
+
+kill $ASHERAH_JAVA_SIDECAR_PID
+rm -rf /tmp/appencryption.sock
--- tests/cross-language/scripts/encrypt_all.sh
+++ tests/cross-language/scripts/encrypt_all.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -e
 
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
