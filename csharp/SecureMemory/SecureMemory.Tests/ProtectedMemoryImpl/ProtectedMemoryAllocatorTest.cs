e8cada5beae29c87e9c2b821432d8f68a0a886f8
Jeremiah Gowdy <jeremiah@gowdy.me>
2020-05-22 09:21:09 -0700

    Clean up impure tests

--- csharp/SecureMemory/SecureMemory.Tests/ProtectedMemoryImpl/ProtectedMemoryAllocatorTest.cs
+++ csharp/SecureMemory/SecureMemory.Tests/ProtectedMemoryImpl/ProtectedMemoryAllocatorTest.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Net.Http.Headers;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using GoDaddy.Asherah.SecureMemory.ProtectedMemoryImpl;
 using GoDaddy.Asherah.SecureMemory.ProtectedMemoryImpl.Libc;
 using GoDaddy.Asherah.SecureMemory.ProtectedMemoryImpl.Linux;
 using GoDaddy.Asherah.SecureMemory.ProtectedMemoryImpl.MacOS;
 using GoDaddy.Asherah.SecureMemory.ProtectedMemoryImpl.Windows;
-using Moq;
 using Xunit;
 
 namespace GoDaddy.Asherah.SecureMemory.Tests.ProtectedMemoryImpl
@@ -39,30 +36,11 @@ namespace GoDaddy.Asherah.SecureMemory.Tests.ProtectedMemoryImpl
             }
         }
 
-        private void CheckIntPtr(IntPtr intPointer, string methodName)
+        private static void CheckIntPtr(IntPtr intPointer, string methodName)
         {
             if (intPointer == IntPtr.Zero || intPointer == InvalidPointer)
             {
                 throw new LibcOperationFailedException(methodName, intPointer.ToInt64());
-            }
-        }
-
-        private void CheckZero(int result, string methodName)
-        {
-            if (result != 0)
-            {
-                // NOTE: Even though this references Win32 it actually returns
-                // the last errno on non-Windows platforms.
-                var errno = Marshal.GetLastWin32Error();
-                throw new LibcOperationFailedException(methodName, result, errno);
-            }
-        }
-
-        private void CheckZero(int result, string methodName, Exception exceptionInProgress)
-        {
-            if (result != 0)
-            {
-                throw new LibcOperationFailedException(methodName, result, exceptionInProgress);
             }
         }
 
@@ -84,6 +62,7 @@ namespace GoDaddy.Asherah.SecureMemory.Tests.ProtectedMemoryImpl
             try
             {
                 protectedMemoryAllocator.SetReadWriteAccess(pointer, 1);
+                CheckIntPtr(pointer, "IProtectedMemoryAllocator.Alloc");
 
                 // Verifies we can write and read back
                 Marshal.WriteByte(pointer, 0, 42);
@@ -99,6 +78,8 @@ namespace GoDaddy.Asherah.SecureMemory.Tests.ProtectedMemoryImpl
         private void TestAllocSuccess()
         {
             IntPtr pointer = protectedMemoryAllocator.Alloc(1);
+            CheckIntPtr(pointer, "IProtectedMemoryAllocator.Alloc");
+
             try
             {
                 // just do some sanity checks
@@ -114,65 +95,6 @@ namespace GoDaddy.Asherah.SecureMemory.Tests.ProtectedMemoryImpl
         [Fact]
         private void TestFree()
         {
-        }
-
-        [Fact]
-        private void TestCheckPointerWithRegularPointerShouldSucceed()
-        {
-            IntPtr pointer = protectedMemoryAllocator.Alloc(1);
-            try
-            {
-                CheckIntPtr(pointer, "blah");
-            }
-            finally
-            {
-                protectedMemoryAllocator.Free(pointer, 1);
-            }
-        }
-
-        [Fact]
-        private void TestCheckPointerWithNullPointerShouldFail()
-        {
-            Assert.Throws<LibcOperationFailedException>(() =>
-            {
-                CheckIntPtr(IntPtr.Zero, "blah");
-            });
-        }
-
-        [Fact]
-        private void TestCheckPointerWithMapFailedPointerShouldFail()
-        {
-            Assert.Throws<LibcOperationFailedException>(() =>
-            {
-                CheckIntPtr(new IntPtr(-1), "blah");
-            });
-        }
-
-        [Fact]
-        private void TestCheckZeroWithZeroResult()
-        {
-            CheckZero(0, "blah");
-        }
-
-        [Fact]
-        private void TestCheckZeroWithNonZeroResult()
-        {
-            Assert.Throws<LibcOperationFailedException>(() => { CheckZero(1, "blah"); });
-        }
-
-        [Fact]
-        private void TestCheckZeroThrowableWithZeroResult()
-        {
-            CheckZero(0, "blah", new InvalidOperationException());
-        }
-
-        [Fact]
-        private void TestCheckZeroThrowableWithNonZeroResult()
-        {
-            Assert.Throws<LibcOperationFailedException>(() =>
-            {
-                CheckZero(1, "blah", new InvalidOperationException());
-            });
         }
     }
 }
