41d29399612d2da21f927490272a9e30b42ebf6a
Jeremiah Gowdy <jgowdy@godaddy.com>
2020-06-04 12:18:33 -0700

    Initial Windows support (#198)
    * Initial Windows support
    
    * Remove unsupported check
    
    * Relocate the pure tests to benefit all IProtectedMemoryAllocator implementations
    
    * Fix mixed up namespaces
    
    * Fix VirtualAllocEx calls to use process handle rather than NULL
    
    * Update tests for Windows
    
    * Clean up impure tests
    
    * Temporarily disable VirtualAllocEx
    
    * Implement different Windows off-heap allocator strategies
    
    * Normalize interop types
    
    * Settled on VirtualAllocEx
    
    * Attempt to stop Windows tests from running on non-Windows builds
    
    * Clean up Windows interop
    
    * Ignore Windows code for codecov calculations
    
    * Switch to compile time debug strategy
    
    * Fix compile time debug strategy
    
    * Implement CryptProtectMemory based protections as an alternative to page protections
    
    * Simplify by using VirtualAlloc/VirtualFree
    
    * Add a HeapAlloc version as well, just in case
    
    * Add locked memory when we are decrypted
    
    * Fix formatting
    
    * Switch to VirtualAlloc
    
    * HeapAlloc implementation flawed
    
    * Remove unused functions
    
    * Formatting
    
    * Reduced inconsistency in error handling
    
    * Fixes
    
    * Fix missed exception types

---
+++ csharp/SecureMemory/PlatformNative/LLP64/Windows/VirtualUnlock.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoDaddy.Asherah.PlatformNative.LLP64.Windows
+{
+    public enum VirtualUnlockErrors
+    {
+        ERROR_NOT_LOCKED = 158,
+    }
+}
---
+++ csharp/SecureMemory/PlatformNative/LLP64/Windows/WindowsInterop.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Runtime.InteropServices;
+using GoDaddy.Asherah.PlatformNative.LLP64.Windows.Enums;
+
+namespace GoDaddy.Asherah.PlatformNative.LLP64.Windows
+{
+    public class WindowsInterop
+    {
+        [DllImport("Kernel32.dll", EntryPoint="RtlZeroMemory", SetLastError=true, ExactSpelling=true)]
+        public static extern void ZeroMemory(IntPtr dest, UIntPtr size);
+
+        [DllImport("kernel32.dll", SetLastError=true, ExactSpelling=true)]
+        public static extern IntPtr VirtualAlloc(IntPtr lpAddress, UIntPtr dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+
+        [DllImport("kernel32.dll", SetLastError=true)]
+        public static extern bool VirtualLock(IntPtr lpAddress, UIntPtr dwSize);
+
+        [DllImport("kernel32.dll", SetLastError=true)]
+        public static extern bool VirtualUnlock(IntPtr lpAddress, UIntPtr dwSize);
+
+        [DllImport("kernel32.dll", SetLastError=true, ExactSpelling=true)]
+        public static extern bool VirtualFree(IntPtr lpAddress, UIntPtr dwSize, AllocationType dwFreeType);
+
+        [DllImport("crypt32.dll", SetLastError=true)]
+        public static extern bool CryptProtectMemory(IntPtr ptr, UIntPtr dwSize, CryptProtectMemoryOptions dwFlags);
+
+        [DllImport("crypt32.dll", SetLastError=true)]
+        public static extern bool CryptUnprotectMemory(IntPtr ptr, UIntPtr dwSize, CryptProtectMemoryOptions dwFlags);
+    }
+}
