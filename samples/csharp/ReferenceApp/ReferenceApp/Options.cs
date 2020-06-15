9b7555c04a578506b16af89dc52d06fc826195d8
Nikhil Lohia <nikoo28@gmail.com>
2020-06-15 15:35:10 -0700

    [C#] : Update reference app and documentation (#228)
    * [C#] : Update reference app and documentation
    
    * Apply suggestions and address review comments
    
    * Bump up AE to v0.1.4

--- samples/csharp/ReferenceApp/ReferenceApp/Options.cs
+++ samples/csharp/ReferenceApp/ReferenceApp/Options.cs
@@ -6,19 +6,31 @@ namespace GoDaddy.Asherah.ReferenceApp
 {
     public class Options
     {
-        [Option('m', "metastore-type", Required = false,  Default = ReferenceApp.Metastore.MEMORY, HelpText = "Type of metastore to use. Enum values: MEMORY, JDBC, DYNAMODB")]
-        public ReferenceApp.Metastore Metastore { get; set; }
+        [Option('m', "metastore-type", Required = false, Default = Metastore.MEMORY, HelpText = "Type of metastore to use. Enum values: MEMORY, ADO, DYNAMODB")]
+        public Metastore Metastore { get; set; }
+
+        [Option('e', "dynamodb-endpoint", Required = false, HelpText = "The DynamoDb service endpoint (only supported by DYNAMODB)")]
+        public string DynamodbEndpoint { get; set; }
+
+        [Option('r', "dynamodb-region", Required = false, HelpText = "The AWS region for DynamoDB requests (only supported by DYNAMODB)")]
+        public string DynamodbRegion { get; set; }
+
+        [Option('t', "dynamodb-table-name", Required = false, HelpText = "The table name for DynamoDb (only supported by DYNAMODB)")]
+        public string DynamodbTableName { get; set; }
+
+        [Option('s', "key-suffix", Required = false, HelpText = "Configure the metastore to use key suffixes (only supported by DYNAMODB)")]
+        public string KeySuffix { get; set; }
 
         [Option('a', "ado-connection-string", Required = false, HelpText = "ADO connection string to use for an ADO metastore. Required for ADO metastore.")]
         public string AdoConnectionString { get; set; }
 
-        [Option('k', "kms-type", Required = false, Default = ReferenceApp.Kms.STATIC, HelpText = "Type of key management service to use. Enum values: STATIC, AWS")]
-        public ReferenceApp.Kms Kms { get; set; }
+        [Option('k', "kms-type", Required = false, Default = Kms.STATIC, HelpText = "Type of key management service to use. Enum values: STATIC, AWS")]
+        public Kms Kms { get; set; }
 
         [Option('p', "preferred-region", Required = false, HelpText = "Preferred region to use for KMS if using AWS KMS. Required for AWS KMS.")]
         public string PreferredRegion { get; set; }
 
-        [Option('r', "region-arn-tuples", Required = false, Separator = ',', HelpText = "Comma separated list of <region>=<kms_arn> tuples. Required for AWS KMS.")]
+        [Option('t', "region-arn-tuples", Required = false, Separator = ',', HelpText = "Comma separated list of <region>=<kms_arn> tuples. Required for AWS KMS.")]
         public IEnumerable<string> RegionToArnTuples { get; set; }
 
         [Option('i', "iterations", Required = false, HelpText = "Number of encrypt/decrypt iterations to run", Default = 1)]
