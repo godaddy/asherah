8229dd88968fe0ed80c350870b60e83aab02fca0
Sushant Mimani <smimani@godaddy.com>
2020-06-16 15:29:38 -0700

    Use symlink for CLTF build

--- tests/cross-language/csharp/scripts/build.sh
+++ tests/cross-language/csharp/scripts/build.sh
@@ -1,6 +1,1 @@
-#!/usr/bin/env bash
-set -e
-
-# Workaround for https://github.com/SpecFlowOSS/SpecFlow/issues/1912#issue-583000545
-export MSBUILDSINGLELOADCONTEXT=1
-dotnet build --configuration Release --no-restore
+../../../../build/csharp/build.sh
