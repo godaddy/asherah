324da92be4aab005ccf0db16aa0d89e22f2c8099
Sushant Mimani <smimani@godaddy.com>
2020-06-17 11:01:56 -0700

    [Go] Move scripts to root level build directory and symlink everywhere (#238)
    * [Go] Move scripts to root level build directory and symlink everywhere

--- go/appencryption/scripts/release_prod.sh
+++ go/appencryption/scripts/release_prod.sh
@@ -1,17 +1,1 @@
-#!/usr/bin/env bash
-set -e
-
-BASE_VERSION=$(cat .versionfile)
-ARTIFACT_NAME='appencryption'
-TAG=`echo go/${ARTIFACT_NAME}/v${BASE_VERSION}`
-
-RESULT=$(git tag -l ${TAG})
-if [[ "$RESULT" != ${TAG}  ]]; then
-    # Create tag
-    echo "Releasing ${ARTIFACT_NAME} artifact"
-    git tag -f ${TAG} ${CIRCLE_SHA1}
-    ssh-agent sh -c 'ssh-add ~/.ssh/id_rsa_git; git push origin --tags'
-    echo "Created tag ${TAG}"
-else
-    echo "${TAG} exists for ${ARTIFACT_NAME} v${BASE_VERSION}"
-fi
+../../../build/go/release_prod.sh
