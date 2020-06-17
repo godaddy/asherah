342feffe76554eea119f089e6f6027b8c54c0453
Sushant Mimani <smimani@godaddy.com>
2020-06-16 17:08:45 -0700

    [Go] Move scripts to root level build directory and symlink everywhere

--- go/securememory/scripts/benchmark_test.sh
+++ go/securememory/scripts/benchmark_test.sh
@@ -1,6 +1,1 @@
-#!/usr/bin/env bash
-set -e
-
-# Looks like CGO has to be enabled for -race if we're using go modules
-# Let benchmark failures fail the build since the plot conversion just renders empty results
-CGO_ENABLED=1 go test ./... -race -run=Bench -bench=. -benchtime=5s -v --tags=race_tests | tee benchmark.out && test ${PIPESTATUS[0]} -eq 0
+../../../build/go/benchmark_test.sh
--- tests/cross-language/go/scripts/build.sh
+++ tests/cross-language/go/scripts/build.sh
@@ -1,2 +1,1 @@
-#!/usr/bin/env bash
-go build ./...
+../../../../build/go/build.sh
