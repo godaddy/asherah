d024bdd25dc8bbbcbf2630961a0ce5cd8f4728ec
Sushant Mimani <smimani@godaddy.com>
2020-06-25 10:49:06 -0700

    Update checkstyle version

--- server/java/src/main/java/com/godaddy/asherah/grpc/DynamoDbConfig.java
+++ server/java/src/main/java/com/godaddy/asherah/grpc/DynamoDbConfig.java
@@ -14,38 +14,18 @@ public class DynamoDbConfig {
     this.tableName = tableName;
   }
 
-  /**
-   * Getter for the field {@code endpointConfig}.
-   *
-   * @return The end point for DynamoDb configuration.
-   */
   public String getEndpointConfig() {
     return endpointConfig;
   }
 
-  /**
-   * Getter for the field {@code region}.
-   *
-   * @return The region for DynamoDb configuration.
-   */
   public String getRegion() {
     return region;
   }
 
-  /**
-   * Getter for the field {@code keySuffix}.
-   *
-   * @return The key suffix to be used for DynamoDb Global Tables.
-   */
   public String getKeySuffix() {
     return keySuffix;
   }
 
-  /**
-   * Getter for the field {@code tableName}.
-   *
-   * @return The DynamoDb table name.
-   */
   public String getTableName() {
     return tableName;
   }
