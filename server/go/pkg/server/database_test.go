ad86964ddca0bec39c4f5c1281e45680735198b8
Jeff Channell <jchannell@godaddy.com>
2020-07-10 10:55:03 -0400

    fix linting

--- server/go/pkg/server/database_test.go
+++ server/go/pkg/server/database_test.go
@@ -18,13 +18,17 @@ func (f *FakeDriver) Open(name string) (driver.Conn, error) {
 
 func TestMain(m *testing.M) {
 	dbdriver = "fake"
+
 	sql.Register("fake", &FakeDriver{})
+
 	os.Exit(m.Run())
 }
 
 func TestNewMysql(t *testing.T) {
 	assert.Nil(t, dbconnection)
+
 	_, err := newMysql("root:secret@(localhost:%s)/mysql")
+
 	assert.Nil(t, err)
 	assert.NotNil(t, dbconnection)
 }
