db905abee1f91f770c2d497433105fadbcc1a6d8
Nikhil Lohia <nikoo28@gmail.com>
2020-07-10 14:13:03 -0700

    Add docs for public methods in C# (#248)
    * WIP : Add documentation in csharp
    
    * WIP : Add docs for 3 packages
    
    * Misc fixes
    
    * Fix build error

--- csharp/AppEncryption/AppEncryption/Envelope/EnvelopeKeyRecord.cs
+++ csharp/AppEncryption/AppEncryption/Envelope/EnvelopeKeyRecord.cs
@@ -8,13 +8,51 @@ using Newtonsoft.Json.Linq;
 
 namespace GoDaddy.Asherah.AppEncryption.Envelope
 {
+    /// <summary>
+    /// The <see cref="EnvelopeKeyRecord"/> format is:
+    /// <code>
+    /// {
+    ///   Created: UTC epoch in seconds of when the key was created Identifier data of parent key (which encrypts this
+    ///            key),
+    ///   ParentKeyMeta: {
+    ///     KeyId: KeyId of the parent key,
+    ///     Created: Created timestamp of parent key
+    ///   },
+    ///   Key: Base64 converted value of "Key encrypted with the parent key",
+    ///   Revoked: The revocation status of the key (True/False)
+    /// }
+    /// </code>
+    /// NOTE: For system key, the parent <see cref="KeyMeta"/> (in this case the master key identifier) may instead be a
+    /// part of the Key content, depending on the master key type.
+    /// </summary>
     public class EnvelopeKeyRecord
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnvelopeKeyRecord"/> class using the provided parameters. An
+        /// envelope key record is an internal data structure used to represent a system key, intermediate key or a data
+        /// row key. It consists of an encrypted key and metadata referencing the parent key in the key hierarchy used
+        /// to encrypt it (i.e. its Key Encryption Key).
+        /// </summary>
+        ///
+        /// <param name="created">Creation time of the <paramref name="encryptedKey"/>.</param>.
+        /// <param name="parentKeyMeta">The <see cref="KeyMeta"/> for encryption keys.</param>
+        /// <param name="encryptedKey">The encrypted key (a system key, intermediate key or a data row key.</param>
         public EnvelopeKeyRecord(DateTimeOffset created,  KeyMeta parentKeyMeta,  byte[] encryptedKey)
             : this(created, parentKeyMeta, encryptedKey, null)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnvelopeKeyRecord"/> class using the provided parameters. An
+        /// envelope key record is an internal data structure used to represent a system key, intermediate key or a data
+        /// row key. It consists of an encrypted key and metadata referencing the parent key in the key hierarchy used
+        /// to encrypt it (i.e. its Key Encryption Key).
+        /// </summary>
+        ///
+        /// <param name="created">Creation time of the <paramref name="encryptedKey"/>.</param>.
+        /// <param name="parentKeyMeta">The <see cref="KeyMeta"/> for encryption keys.</param>
+        /// <param name="encryptedKey">The encrypted key (a system key, intermediate key or a data row key.</param>
+        /// <param name="revoked">The revocation status of the encrypted key.</param>
         public EnvelopeKeyRecord(DateTimeOffset created,  KeyMeta parentKeyMeta,  byte[] encryptedKey,  bool? revoked)
         {
             Created = created;
@@ -23,6 +61,10 @@ namespace GoDaddy.Asherah.AppEncryption.Envelope
             Revoked = revoked ?? Option<bool>.None;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnvelopeKeyRecord"/> class.
+        /// </summary>
+        /// <param name="sourceJson">The source json file to use.</param>
         public EnvelopeKeyRecord(Json sourceJson)
         {
             Created = sourceJson.GetDateTimeOffset("Created");
@@ -39,6 +81,19 @@ namespace GoDaddy.Asherah.AppEncryption.Envelope
 
         internal byte[] EncryptedKey { get; }
 
+        /// <summary>
+        /// Converts the <see cref="EnvelopeKeyRecord"/> to a <see cref="JObject"/> format:
+        /// <code>
+        /// {
+        ///   "Created": Creation time of the encrypted key,
+        ///   "ParentKeyMeta": Parent key meta of the encrypted key(if present),
+        ///   "Key": Encrypted Key,
+        ///   "Revoked": True/False
+        /// }
+        /// </code>
+        /// </summary>
+        ///
+        /// <returns>The <see cref="EnvelopeKeyRecord"/> converted to a <see cref="JObject"/>.</returns>
         public JObject ToJson()
         {
             Json json = new Json();
