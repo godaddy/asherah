0c2e9c68db8a4e6a97e4917ad172f0ff010c2bab
Nikhil Lohia <nikoo28@gmail.com>
2020-07-13 13:57:49 -0700

    Use TestUtils to create payload and sessionFactory

--- java/app-encryption/src/it/java/com/godaddy/asherah/regression/CrossPartitionDecryptIT.java
+++ java/app-encryption/src/it/java/com/godaddy/asherah/regression/CrossPartitionDecryptIT.java
@@ -4,55 +4,33 @@ import com.godaddy.asherah.TestSetup;
 import com.godaddy.asherah.appencryption.Session;
 import com.godaddy.asherah.appencryption.SessionFactory;
 import com.godaddy.asherah.appencryption.exceptions.MetadataMissingException;
-import com.godaddy.asherah.appencryption.kms.StaticKeyManagementServiceImpl;
-import com.godaddy.asherah.appencryption.persistence.InMemoryMetastoreImpl;
-import com.godaddy.asherah.appencryption.persistence.Metastore;
-import com.godaddy.asherah.crypto.NeverExpiredCryptoPolicy;
-import org.json.JSONObject;
+import com.godaddy.asherah.utils.PayloadGenerator;
+import com.godaddy.asherah.utils.SessionFactoryGenerator;
 import org.junit.jupiter.api.Test;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CrossPartitionDecryptIT {
 
   @Test
   void testCrossPartitionDecryptShouldFail() {
-    String dataRowString;
-    String originalPayloadString;
+    byte[] payload = PayloadGenerator.createDefaultRandomBytePayload();
+    byte[] dataRowRecordBytes;
 
-    Metastore<JSONObject> metastore = TestSetup.createMetastore();
+    String originalPartitionId = "shopper123";
+    String alternatePartitionId = "shopper1234";
 
-    // Encrypt originalPayloadString with partition "shopper123"
-    try (SessionFactory sessionFactory = SessionFactory
-      .newBuilder("productId", "test")
-      .withMetastore(metastore)
-      .withCryptoPolicy(new NeverExpiredCryptoPolicy())
-      .withKeyManagementService(TestSetup.createKeyManagemementService())
-      .build()) {
+    SessionFactory sessionFactory = SessionFactoryGenerator
+      .createDefaultSessionFactory(TestSetup.createKeyManagemementService(), TestSetup.createMetastore());
 
-      try (Session<byte[], byte[]> sessionBytes = sessionFactory
-        .getSessionBytes("shopper123")) {
+    // Encrypt with originalPartitionId
+    try (Session<byte[], byte[]> sessionBytes = sessionFactory.getSessionBytes(originalPartitionId)) {
+      dataRowRecordBytes = sessionBytes.encrypt(payload);
+    }
 
-        originalPayloadString = "mysupersecretpayload";
-
-        byte[] dataRowRecordBytes =
-          sessionBytes.encrypt(originalPayloadString.getBytes(StandardCharsets.UTF_8));
-
-        // Consider this us "persisting" the DRR
-        dataRowString = Base64.getEncoder().encodeToString(dataRowRecordBytes);
-      }
-
-      // Decrypt dataRowString with partition "shopper12345"
-      try (Session<byte[], byte[]> sessionBytes = sessionFactory
-        .getSessionBytes("shopper12345")) {
-
-        byte[] newDataRowRecordBytes = Base64.getDecoder().decode(dataRowString);
-
-        assertThrows(MetadataMissingException.class, () -> sessionBytes.decrypt(newDataRowRecordBytes));
-      }
+    // Decrypt dataRowString with alternatePartitionId
+    try (Session<byte[], byte[]> sessionBytes = sessionFactory.getSessionBytes(alternatePartitionId)) {
+      assertThrows(MetadataMissingException.class, () -> sessionBytes.decrypt(dataRowRecordBytes));
     }
   }
 }
