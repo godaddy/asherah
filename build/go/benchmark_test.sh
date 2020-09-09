554b2b5df25530c2eef15d890b9c750cf5552860
Bo Thompson <ethompson@godaddy.com>
2020-09-08 17:05:07 -0700

    [go] print resource limits from benchmark_test.sh

--- build/go/benchmark_test.sh
+++ build/go/benchmark_test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# dump the current resource limits
+ulimit -a
+
 # Looks like CGO has to be enabled for -race if we're using go modules
 # Let benchmark failures fail the build since the plot conversion just renders empty results
 CGO_ENABLED=1 go test ./... -race -run=Bench -bench=. -benchtime=5s -v --tags=race_tests | tee benchmark.out && test ${PIPESTATUS[0]} -eq 0
