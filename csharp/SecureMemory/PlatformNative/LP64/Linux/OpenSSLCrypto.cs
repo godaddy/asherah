ab3a157547a6befd8a46fbeaf3fe316768471ad7
Jeremiah Gowdy <jeremiah@gowdy.me>
2020-09-18 09:33:50 -0700

    Remove commented code

--- csharp/SecureMemory/PlatformNative/LP64/Linux/OpenSSLCrypto.cs
+++ csharp/SecureMemory/PlatformNative/LP64/Linux/OpenSSLCrypto.cs
@@ -18,8 +18,6 @@ namespace GoDaddy.Asherah.PlatformNative.LP64.Linux
         public const int EVP_MAX_KEY_LENGTH = 64;
         public const int EVP_MAX_IV_LENGTH = 16;
 
-        // (inl + cipher_block_size - 1)
-        // EVP_EncryptUpdate() encrypts inl bytes from the buffer in and writes the encrypted version to out. This function can be called multiple times to encrypt successive blocks of data. The amount of data written depends on the block alignment of the encrypted data: as a result the amount of data written may be anything from zero bytes to (inl + cipher_block_size - 1) so out should contain sufficient room. The actual number of bytes written is placed in outl.
         [DllImport("libcrypto.so.1.1", EntryPoint = "EVP_CIPHER_CTX_new", SetLastError = true)]
         private static extern IntPtr _EVP_CIPHER_CTX_new();
 
@@ -163,78 +161,5 @@ namespace GoDaddy.Asherah.PlatformNative.LP64.Linux
         {
             return _EVP_CIPHER_iv_length(e);
         }
-
-        /*
-         *              int EVP_CIPHER_block_size(const EVP_CIPHER *e);
-             int EVP_CIPHER_key_length(const EVP_CIPHER *e);
-             int EVP_CIPHER_iv_length(const EVP_CIPHER *e);
-         */
-        /*
-             #include <openssl/evp.h>
-
-             EVP_CIPHER_CTX *EVP_CIPHER_CTX_new(void);
-             int EVP_CIPHER_CTX_reset(EVP_CIPHER_CTX *ctx);
-             void EVP_CIPHER_CTX_free(EVP_CIPHER_CTX *ctx);
-
-             int EVP_EncryptInit_ex(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
-                                    ENGINE *impl, const unsigned char *key, const unsigned char *iv);
-             int EVP_EncryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out,
-                                   int *outl, const unsigned char *in, int inl);
-             int EVP_EncryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl);
-
-             int EVP_DecryptInit_ex(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
-                                    ENGINE *impl, const unsigned char *key, const unsigned char *iv);
-             int EVP_DecryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out,
-                                   int *outl, const unsigned char *in, int inl);
-             int EVP_DecryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *outm, int *outl);
-
-             int EVP_CipherInit_ex(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
-                                   ENGINE *impl, const unsigned char *key, const unsigned char *iv, int enc);
-             int EVP_CipherUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out,
-                                  int *outl, const unsigned char *in, int inl);
-             int EVP_CipherFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *outm, int *outl);
-
-             int EVP_EncryptInit(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
-                                 const unsigned char *key, const unsigned char *iv);
-             int EVP_EncryptFinal(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl);
-
-             int EVP_DecryptInit(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
-                                 const unsigned char *key, const unsigned char *iv);
-             int EVP_DecryptFinal(EVP_CIPHER_CTX *ctx, unsigned char *outm, int *outl);
-
-             int EVP_CipherInit(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
-                                const unsigned char *key, const unsigned char *iv, int enc);
-             int EVP_CipherFinal(EVP_CIPHER_CTX *ctx, unsigned char *outm, int *outl);
-
-             int EVP_CIPHER_CTX_set_padding(EVP_CIPHER_CTX *x, int padding);
-             int EVP_CIPHER_CTX_set_key_length(EVP_CIPHER_CTX *x, int keylen);
-             int EVP_CIPHER_CTX_ctrl(EVP_CIPHER_CTX *ctx, int type, int arg, void *ptr);
-             int EVP_CIPHER_CTX_rand_key(EVP_CIPHER_CTX *ctx, unsigned char *key);
-
-             const EVP_CIPHER *EVP_get_cipherbyname(const char *name);
-             const EVP_CIPHER *EVP_get_cipherbynid(int nid);
-             const EVP_CIPHER *EVP_get_cipherbyobj(const ASN1_OBJECT *a);
-
-             int EVP_CIPHER_nid(const EVP_CIPHER *e);
-             int EVP_CIPHER_block_size(const EVP_CIPHER *e);
-             int EVP_CIPHER_key_length(const EVP_CIPHER *e);
-             int EVP_CIPHER_iv_length(const EVP_CIPHER *e);
-             unsigned long EVP_CIPHER_flags(const EVP_CIPHER *e);
-             unsigned long EVP_CIPHER_mode(const EVP_CIPHER *e);
-             int EVP_CIPHER_type(const EVP_CIPHER *ctx);
-
-             const EVP_CIPHER *EVP_CIPHER_CTX_cipher(const EVP_CIPHER_CTX *ctx);
-             int EVP_CIPHER_CTX_nid(const EVP_CIPHER_CTX *ctx);
-             int EVP_CIPHER_CTX_block_size(const EVP_CIPHER_CTX *ctx);
-             int EVP_CIPHER_CTX_key_length(const EVP_CIPHER_CTX *ctx);
-             int EVP_CIPHER_CTX_iv_length(const EVP_CIPHER_CTX *ctx);
-             void *EVP_CIPHER_CTX_get_app_data(const EVP_CIPHER_CTX *ctx);
-             void EVP_CIPHER_CTX_set_app_data(const EVP_CIPHER_CTX *ctx, void *data);
-             int EVP_CIPHER_CTX_type(const EVP_CIPHER_CTX *ctx);
-             int EVP_CIPHER_CTX_mode(const EVP_CIPHER_CTX *ctx);
-
-             int EVP_CIPHER_param_to_asn1(EVP_CIPHER_CTX *c, ASN1_TYPE *type);
-             int EVP_CIPHER_asn1_to_param(EVP_CIPHER_CTX *c, ASN1_TYPE *type);
-         */
     }
 }
