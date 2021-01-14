f792235724a45d7d0b4e0fc23cb6e3bfe5509f3a
Bo Thompson <bo@godaddy.com>
2021-01-14 14:37:50 -0700

    ensure `out` directory exists

--- samples/go/aws/lambda/1-create-bucket.sh
+++ samples/go/aws/lambda/1-create-bucket.sh
@@ -3,5 +3,7 @@
 BUCKET_ID=$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
 BUCKET_NAME=lambda-artifacts-$BUCKET_ID
 
+mkdir -p out
+
 echo $BUCKET_NAME > out/bucket-name.txt
 aws s3 mb s3://$BUCKET_NAME
