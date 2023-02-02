8e5c0f066752179f82f57002cac2612b4506f017
Bo Thompson <bo@godaddy.com>
2023-02-02 12:54:41 -0700

    grammar++

--- go/appencryption/pkg/persistence/sql_test.go
+++ go/appencryption/pkg/persistence/sql_test.go
@@ -8,7 +8,7 @@ import (
 
 // SQLSuite provides unit tests for the sql metastore implementation.
 //
-// Note that the integration tests formally provided by this package have been relocated to
+// Note that the integration tests formerly provided by this package have been relocated to
 // a new dedicated module: github.com/godaddy/asherah/go/appencryption/integrationtest.
 type SQLSuite struct {
 	suite.Suite
