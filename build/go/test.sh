b0b0490759407cd387bb73205bd9a40b72a52fca
Bo Thompson <bo@godaddy.com>
2023-05-04 16:06:13 -0700

    remove redundant env variable (CGO_ENABLED)

--- build/go/test.sh
+++ build/go/test.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 #Using https://github.com/gotestyourself/gotestsum for reference
 
-# Looks like CGO has to be enabled for -race if we're using go modules
-CGO_ENABLED=1 gotestsum -f testname --junitfile junit_results.xml -- -race -coverprofile coverage.out -v ./...
+gotestsum -f testname --junitfile junit_results.xml -- -race -coverprofile coverage.out -v ./...
