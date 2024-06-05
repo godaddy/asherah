a1cc0a6824c07e5f75c907ad61eb083415cfda3d
Bo Thompson <bo@godaddy.com>
2024-06-04 17:04:01 -0700

    removed redundant status check

--- build/go/lint.sh
+++ build/go/lint.sh
@@ -5,12 +5,3 @@ curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/insta
 ./bin/golangci-lint --version
 
 ./bin/golangci-lint run --config .golangci.yml
-
-# golint is designed to return zero even it finds lint
-# https://github.com/golang/lint/issues/65
-# Hence we check if the lint command produced any output. If no output, then no errors were found
-if [[ $(./bin/golangci-lint run --config .golangci.yml) ]]; then
-    exit 1
-else
-    exit 0
-fi
