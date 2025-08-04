5299fc0161dfa0951c0141ad9322005d69e04553
Jeremiah Gowdy <jgowdy@godaddy.com>
2025-08-03 18:18:39 -0700

    Fix linting issues
    - Fix gofmt formatting in policy.go
    - Fix gci import ordering in policy_test.go

--- go/appencryption/policy_test.go
+++ go/appencryption/policy_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestNewCryptoPolicy_DefaultEvictionPolicies(t *testing.T) {
 	policy := NewCryptoPolicy()
-	
+
 	// Verify that the default eviction policies are set to LRU instead of empty/simple
 	assert.Equal(t, "lru", policy.IntermediateKeyCacheEvictionPolicy, "IntermediateKeyCacheEvictionPolicy should default to lru")
 	assert.Equal(t, "lru", policy.SystemKeyCacheEvictionPolicy, "SystemKeyCacheEvictionPolicy should default to lru")
 	assert.Equal(t, "slru", policy.SessionCacheEvictionPolicy, "SessionCacheEvictionPolicy should default to slru")
-	
+
 	// Verify other defaults are still set correctly
 	assert.Equal(t, DefaultKeyCacheMaxSize, policy.IntermediateKeyCacheMaxSize)
 	assert.Equal(t, DefaultKeyCacheMaxSize, policy.SystemKeyCacheMaxSize)
@@ -24,7 +24,7 @@ func TestCryptoPolicy_CanOverrideEvictionPolicy(t *testing.T) {
 	policy := NewCryptoPolicy()
 	policy.SystemKeyCacheEvictionPolicy = "simple"
 	policy.IntermediateKeyCacheEvictionPolicy = "simple"
-	
+
 	assert.Equal(t, "simple", policy.SystemKeyCacheEvictionPolicy)
 	assert.Equal(t, "simple", policy.IntermediateKeyCacheEvictionPolicy)
 }
